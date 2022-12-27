469b52541a37f81a2d52e2060f38125103b8c868
Michał Rudzki <michal@mailx.com.pl>
2022-12-27 20:21:50 +0100

    fixing if statment for checking no. of elements

--- packages.py
+++ packages.py
@@ -18,19 +18,21 @@ def summary():
 element_count = input("Ile elementów: ")
 packages_weight = input("Wagi elementów: ")
 all_weight_per_package = all_elements_weight(packages_weight)
-
-for weight in all_weight_per_package:
-    if len(tmp_packages) == 0:
-        tmp_packages.append(weight)
-        all_packages = [tmp_packages]
-    elif sum(tmp_packages) + weight <= 20:
-        all_packages[packages_count].append(weight)
-    elif sum(tmp_packages) + weight >= 20:
-        packages_count += 1
-        all_packages.append([weight])
+if len(all_weight_per_package) == element_count:
+    for weight in all_weight_per_package:
+        if len(tmp_packages) == 0:
+            tmp_packages.append(weight)
+            all_packages = [tmp_packages]
+        elif sum(tmp_packages) + weight <= 20:
+            all_packages[packages_count].append(weight)
+        elif sum(tmp_packages) + weight >= 20:
+            packages_count += 1
+            all_packages.append([weight])
+else:
+    print("Za mała lista elementów.")
 
 print(f"\nPODSUMOWANIE:")
-print(f"Wysłano <ilosc_paczek> (waga1+waga2)")
+print(f"Wysłano {len(all_packages)} (waga1+waga2)")
 print(f"Wysłano: {sum(all_weight_per_package)}kg")
 print(f"Suma pustych kilogramow: YYkg")
 print(f"Najwięcej pustych kilogramów ma paczka <numer_paczki> (waga)")
