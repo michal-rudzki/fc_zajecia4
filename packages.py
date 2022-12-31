fb031a9d16a75805e7c79195d5c16f0652c0fb84
Michał Rudzki <michal@mailx.com.pl>
2022-12-31 14:39:10 +0100

    summary definition have been moved to the pack.py file

--- packages.py
+++ packages.py
@@ -23,28 +23,10 @@ def main():
             elif sum(all_packages[packages_count]) + weight >= PACKAGE_LIMIT:
                 packages_count += 1
                 all_packages.append([weight])
-
-        print(f"\nPODSUMOWANIE:")
-
-        for package in range(len(all_packages)):
-            if len(weight_per_package) != 0:
-                weight_per_package += ", "
-            tmp = ""
-            index = 0
-            for weight in all_packages[package]:
-                tmp += str(weight)
-                if index < len(all_packages[package])-1:
-                    tmp += "+"
-                    index += 1
-                else:
-                    weight_per_package += tmp
-
-        print(f"Wysłano {len(all_packages)} paczki ({weight_per_package})")
-        print(f"Wysłano: {sum(all_weight_per_package)}kg")
-        print(f"Suma pustych kilogramow: {sum_of_empty_weight(all_packages)}kg")
-        print(f"Najwięcej pustych kilogramów ma paczka {check_empty_package_weight(sum_of_empty_weight(all_packages, False))}")
+        summary(weight_per_package, all_packages)
     else:
-        print("Sprawdź listę elementów.")
+        print("Za mało elementów, sprawdź listę elementów.")
+
 
 if __name__ == "__main__":
     main()
